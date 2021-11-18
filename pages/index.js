ca0c7ea1c7f5d807825959c13053148e0dd4d29d
Menchu MV <mcmvillalobos@gmail.com>
2021-11-18 14:03:57 +0100

    Delete function for post

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,50 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 import PostCard from "../components/PostCard/PostCard";
 import styles from "./index.module.css";
 
-const Home = ({ posts }) => (
-  <div className={styles.container}>
-    <Head>
-      <title>Blog Next</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = ({ posts }) => {
+  const [postList, setPostList] = useState(posts);
 
-    <main>
-      <h1>Posts</h1>
-      <ul className={styles.postList}>
-        {posts.map((post) => (
-          <Link
-            href={`/${post.id}`}
-            key={post.id}
-            passHref
-            className={styles.postLinks}
-          >
-            <a>
-              <PostCard post={post} />
-            </a>
-          </Link>
-        ))}
-      </ul>
-    </main>
-  </div>
-);
+  const onDelete = async (id, event) => {
+    event.preventDefault();
+    await fetch(`https://isdi-blog-posts-api.herokuapp.com/posts/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPostList(postList.filter((post) => post.id !== id));
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Blog Next</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Posts</h1>
+        <ul className={styles.postList}>
+          {postList.map((post) => (
+            <Link
+              href={`/${post.id}`}
+              key={post.id}
+              passHref
+              className={styles.postLinks}
+            >
+              <a>
+                <PostCard post={post} onDelete={onDelete} />
+              </a>
+            </Link>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
 
 export async function getServerSideProps() {
   const res = await fetch("https://isdi-blog-posts-api.herokuapp.com/posts");
