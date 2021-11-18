d9f7ac8edf70af414e538d8f38987954305b203a
Menchu MV <mcmvillalobos@gmail.com>
2021-11-18 12:16:16 +0100

    Added login page

---
+++ components/Login/Login.js
@@ -0,0 +1,70 @@
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from "react";
+import styles from "../../pages/index.module.css";
+
+const Login = () => {
+  const initialValues = {
+    name: "",
+    imageURL: "",
+  };
+  const [userData, setUserData] = useState(initialValues);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const onChange = (event) => {
+    setUserData({ ...userData, [event.target.id]: event.target.value });
+  };
+
+  useEffect(() => {
+    if (userData.name !== "" && userData.imageURL !== "") {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [userData]);
+
+  const onLogin = async (event) => {
+    event.preventDefault();
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  return (
+    <form
+      className={styles.loginform}
+      noValidate
+      autoComplete="off"
+      onSubmit={onLogin}
+    >
+      <h5 className="login-title">Login</h5>
+      <div className="form-inputs">
+        <input
+          type="text"
+          className="input input--name"
+          id="name"
+          placeholder="Name"
+          value={userData.name}
+          onChange={(event) => onChange(event)}
+        />
+
+        <input
+          type="text"
+          className="input input--imageURL"
+          id="imageURL"
+          placeholder="Avatar URL"
+          value={userData.imageURL}
+          onChange={(event) => onChange(event)}
+        />
+
+        <button type="submit" className="login-btn" disabled={isDisabled}>
+          LOGIN
+        </button>
+        {userData.imageURL ? (
+          <img src={userData.imageURL} alt="" height="100" />
+        ) : (
+          ""
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Login;
---
+++ pages/login/index.js
@@ -0,0 +1,9 @@
+import Login from "../../components/Login/Login";
+
+const LoginPage = () => (
+  <main>
+    <h1>Login</h1>
+    <Login />
+  </main>
+);
+export default LoginPage;
