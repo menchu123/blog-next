b631aed54df914760c714ba73a00500cfbc3b984
Menchu MV <mcmvillalobos@gmail.com>
2021-11-18 13:19:35 +0100

    Added user data to post and added some styles

--- components/PostCard/PostCard.js
+++ components/PostCard/PostCard.js
@@ -1,9 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from "./PostCard.module.css";
 
 const PostCard = ({ post }) => (
   <li key={post.id} className={styles.listItem}>
-    <h3>{post.title}</h3>
-    <div>{post.body}</div>
+    <div className={styles.postdata}>
+      <h3>{post.title}</h3>
+      <div>{post.body}</div>
+    </div>
+    <div className={styles.postauthor}>
+      <img
+        src={
+          post.userAvatar
+            ? post.userAvatar
+            : "https://www.clickz.com/wp-content/uploads/2016/03/anontumblr-300x271.png"
+        }
+        alt=""
+        height="100"
+        className={styles.postimage}
+      />
+      <span>{post.userName}</span>
+    </div>
   </li>
 );
 
